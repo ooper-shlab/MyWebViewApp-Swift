ac7d65781ed2a588c0529d362295521ba40441be
OOPer <nagata@shlab.jp>
2015-08-19 20:20:53 +0900

    Initial Commit

---
+++ MyWebViewApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyWebViewApp
+//
+//  Created by 開発 on 2015/8/19.
+//  Copyright © 2015 OOPer (NAGATA, Atsuyuki). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
